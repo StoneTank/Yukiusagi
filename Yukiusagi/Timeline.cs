03c219eb88861d9bff8540ad566b970cf4f78c7f
Taishi <30625424+taishi1@users.noreply.github.com>
2017-08-04 22:39:57 +0900

    add Timeline.cs

---
+++ Yukiusagi/Timeline.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoneTank.Yukiusagi
+{
+    /// <summary>
+    /// タイムラインのプロパティを管理します
+    /// </summary>
+    [Serializable]
+    public class TimelinePropertySet
+    {
+        /// <summary>タイムラインの種類</summary>
+        public TimelineType Type { get; set; }
+
+        /// <summary>種類が Home または Mentions の場合、受信アカウントのID</summary>
+        public List<long> AccountIds { get; set; }
+
+        /// <summary>種類が User の場合、当該ユーザID</summary>
+        public long? UserId { get; set; }
+
+        /// <summary>種類が Search の場合、検索ワード</summary>
+        public string SearchKeyword { get; set; }
+    }
+
+    /// <summary>
+    /// タイムラインの種類
+    /// </summary>
+    public enum TimelineType
+    {
+        Home,
+        Mentions,
+        Messages, // reserved
+        List, // reserved
+        User,
+        Search
+    }
+}
